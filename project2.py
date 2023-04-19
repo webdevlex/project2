3a84b99b9fd54fd9e395e29a9750c7d7f2fb9d80
Nadeem Maida <84353378+NADEE-MJ@users.noreply.github.com>
2023-04-18 19:52:04 -0700

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,60 +1,60 @@
-rows = {'a': 0, '+': 1, '-': 2, '*': 3, '/': 4, '(': 5, ')': 6, '$': 7}
-columns = {'E': 0, 'Q': 1, 'T': 2, 'R': 3, 'F': 4}
+columns = {"a": 0, "+": 1, "-": 2, "*": 3, "/": 4, "(": 5, ")": 6, "$": 7}
+rows = {"E": 0, "Q": 1, "T": 2, "R": 3, "F": 4}
 table = [
     ["TQ", None, None, None, None, "TQ", None, None],
-    [None, "+TQ", "-TQ", None, None, None, '', ''],
+    [None, "+TQ", "-TQ", None, None, None, "", ""],
     ["FR", None, None, None, None, "FR", None, None],
-    [None, '', '', "*FR", "/FR", None, '', ''],
-    ["a", None, None, None, None, "(E)", None, None]
+    [None, "", "", "*FR", "/FR", None, "", ""],
+    ["a", None, None, None, None, "(E)", None, None],
 ]
 
-def print_results(input_out, stack, result):
-    print("Input:", input_out)
-    print("Stack:", end='')
-    for item in stack:
-        print(item)
-    print("Output:", result)
+
+def print_results(initial_input, stack_steps, result_string):
+    print(f"Input: {initial_input}")
+    print("Stack: ", end="")
+    for stack_step in stack_steps:
+        print(stack_step)
+    print(f"Output: {result_string}")
 
 
 def check_valid(input):
-    start = "E$"
-    input_out = input
-    stack = []
-    result = None
+    current_stack = "E$"
+    input_to_process = input
+    stack_steps = []
+    result_string = "String is not accepted/ In valid."
 
-    done = False
-    while not done:
-        s1 = start[0]
-        i1 = input[0]
+    while True:
+        first_of_stack = current_stack[0]
+        first_of_input = input_to_process[0]
 
-        production = None
-        if s1 not in rows:
-            production = table[columns[s1]][rows[i1]]
+        action = None
+        if first_of_stack not in columns.keys():
+            action = table[rows[first_of_stack]][columns[first_of_input]]
 
-        if start[0] == '$' and input[0] == '$':
-            stack.append([start, input, "Accepted"])
-            result = 'String is accepted/ valid.'
-            done = True
-        elif s1 == i1:
-            stack.append([start, input, "Reduce"])
-            start = start.replace(s1, '', 1)
-            input = input.replace(i1, '', 1)
-        elif production or production == '':
-            stack.append([start, input, production])
-            start = start.replace(start[0], production, 1)
+        if current_stack[0] == "$" and input_to_process[0] == "$":
+            stack_steps.append([current_stack, input_to_process, "Accepted"])
+            result_string = "String is accepted/ valid."
+            break
+        elif first_of_stack == first_of_input:
+            stack_steps.append([current_stack, input_to_process, "Reduce"])
+            current_stack = current_stack.replace(first_of_stack, "", 1)
+            input_to_process = input_to_process.replace(first_of_input, "", 1)
+        elif action or action == "":
+            stack_steps.append([current_stack, input_to_process, action])
+            current_stack = current_stack.replace(current_stack[0], action, 1)
         else:
-            result = 'String is not accepted/ In valid.'
-            done = True
+            break
 
-    print_results(input_out, stack, result)
+    print_results(input, stack_steps, result_string)
+
 
 def main():
     # ---- Test Inputs ----
-    input = "(a+a)*a$"
-    # input = "a*(a/a)$"
-    # input = "a(a+a)$"
+    inputs = ["(a+a)*a$", "a*(a/a)$", "a(a+a)$"]
+    # inputs = ["(a+a)*a$"]
+    for input in inputs:
+        check_valid(input)
 
-    check_valid(input)
 
 if __name__ == "__main__":
     main()
